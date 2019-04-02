66f9c0ec6dd08d317f40b44d43f90be244679287
[wangsenping] <[110153917@qq.com]>
2019-04-02 14:05:27 +0800

    Daily exercises

---
+++ Unit Testing/Unit Testing.py
@@ -0,0 +1,18 @@
+# -*- conding:utf-8 -*-
+import unittest
+class TestStringMethods(unittest.TestCase):
+    def test_upper(self):
+        self.assertEqual('xiaoli'.upper(),'XIAOLI')  #判断两个值是否相等
+
+    def test_isupper(self):
+        self.assertTrue('XIAOLI'.isupper())  #判断值是否为True
+        self.assertFalse('xiaoli'.isupper())   #判断值是否为False
+
+    def test_split(self):
+        s = 'hello world'
+        self.assertEqual(s.split(),['hello','world'])
+        with self.assertRaises(TypeError):   # p判断是否异常
+            s.split(2)
+
+        if __name__ == '__main__':
+            unittest.main()
