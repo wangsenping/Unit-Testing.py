0cefdd06ad8c02ca3f8528aa6b35e853499485ca
wangsenping <38548779+wangsenping@users.noreply.github.com>
2019-04-02 14:28:30 +0800

    Update 日常练习7.py

--- 日常练习7.py
+++ 日常练习7.py
@@ -5,12 +5,4 @@ import json
 def send_post(url=None,data=None):
     result = requests.post(url=url, data=data), json()  # 参数必须按照url，data顺序传入
     res = json.dumps(result,ensure_ascii=False,sort_keys=True,indent=2)
-      return(res)
-
-if__name__=='__main__'
-url = 'http://10.10.100.2:8239/login'
-data={"phone":"13271974882","pwd":"wsp12345"}
-
-post = send_post(url=url,data=data)
-
-
+    print（res）
